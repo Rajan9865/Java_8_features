0e6bc6ff5ba3b862f17757e79b7bb8cce2413e44
Rajan9865 <rajan78800@gmail.com>
2023-04-18 23:28:55 +0530

    first commit

---
+++ src/Functional_interface_examples/FunctionalInterfaceExample.java
@@ -0,0 +1,16 @@
+package Functional_interface_examples;
+
+@FunctionalInterface  
+interface AddInterface{  
+    void add(int a, int b);  
+}  
+ 
+public class FunctionalInterfaceExample implements AddInterface {
+	public void add(int a, int b){  
+        System.out.println(a+b);  
+    } 
+	public static void main(String args[]){
+		FunctionalInterfaceExample fie = new FunctionalInterfaceExample();  
+        fie.add(155, 200);  
+	}
+}
