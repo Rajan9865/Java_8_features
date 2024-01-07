eb5a4283632d04e9de0a81738835929ff0f7024c
Rajan9865 <rajan78800@gmail.com>
2024-01-07 07:44:37 +0530

    Lamdba example by javaGuide

---
+++ src/java_guide/RunnableLamdbaExample.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package java_guide;
+
+/**
+ * @author deby7
+ *8:49:59 pm
+ *2024
+ *Java_8_Features_examples
+ *TODO
+ */
+class ThreadDemo implements Runnable
+{
+
+	@Override
+	public void run() {
+		System.out.println("Run methods called ..........");
+	}
+	
+}
+public class RunnableLamdbaExample {
+	public static void main(String[] args) {
+		/*
+		Thread thread=new Thread(new ThreadDemo());
+		thread.start();
+		Runnable runnable=()-> System.out.println("run methods called using lamdba .....");
+		Thread threadLamdba=new Thread(runnable);
+		threadLamdba.start();
+		*/
+		Thread thread=new Thread(new ThreadDemo());
+		thread.start();
+		Thread threadLamdba=new Thread(()-> System.out.println("run methods called using lamdba ...."));
+		threadLamdba.start();
+	}
+
+}
