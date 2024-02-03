f059de8914831b343f1c9b4558bf76bbbd2a1afc
Rajan9865 <rajan78800@gmail.com>
2024-02-03 18:44:36 +0530

    java Guide lecture practice

---
+++ src/java_guide/FunctionalDemo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package java_guide;
+
+import java.util.function.Function;
+
+/**
+ * @author deby7
+ *6:41:04 pm
+ *2024
+ *Java_8_Features_examples
+ *TODO
+ */
+
+class FunctionalImpl implements Function<String, Integer>
+{
+
+	@Override
+	public Integer apply(String t) {
+		return null;
+	}
+	
+}
+public class FunctionalDemo {
+
+}
