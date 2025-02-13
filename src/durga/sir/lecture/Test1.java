64f012ae9bca98f6500020e034b0fdadfa058f0a
Rajan9865 <rajan78800@gmail.com>
2025-02-14 00:04:56 +0530

    practise

---
+++ src/durga/sir/lecture/Test1.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package durga.sir.lecture;
+
+/**
+ * durga.sir.lecture
+ *DelL
+ */
+public class Test1 {
+public static void main(String[] args) {
+	int a=99;
+	int b=7;
+	System.out.println("a | b = " + (a | b));
+}
+}
