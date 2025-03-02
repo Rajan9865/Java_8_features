f29cd4f55ad2910fe43bcdd0540a05b55b3816e7
Rajan9865 <rajan78800@gmail.com>
2025-03-02 09:11:28 +0530

    practise

---
+++ src/interview/potd/MinimumSizeSubarraySum.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package interview.potd;
+
+/**
+ * interview.potd
+ * 
+ * @author Rajan kumar
+ */
+public class MinimumSizeSubarraySum {
+	public static void main(String[] args) {
+		int targer = 78;
+		int[] num = { 2, 3, 1, 2, 4, 3 };
+		System.out.println("minimum subarray : " + minSubArrayLen(num, targer));
+	}
+
+	/**
+	 * @param num
+	 * @param targer
+	 * @return
+	 */
+	private static int minSubArrayLen(int[] num, int targer) {
+		int i=0;
+		int j=0;
+		int sum=0;
+		int minLength=Integer.MAX_VALUE;
+		while (j<num.length) {
+			sum+=num[j];
+			while (sum>=targer) {
+				minLength=Math.min(minLength, j-i+1);
+				sum-=num[i];
+				i++;
+			}
+			j++;
+		}
+		return minLength==Integer.MAX_VALUE?0:minLength;
+	}
+
+}
