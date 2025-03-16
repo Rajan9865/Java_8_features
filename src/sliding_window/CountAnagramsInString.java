36259d6849c5b164f237807ed7bc71209692ad8f
Rajan9865 <rajan78800@gmail.com>
2025-03-16 22:50:56 +0530

    practise

--- src/sliding_window/CountAnagramsInString.java
+++ src/sliding_window/CountAnagramsInString.java
@@ -3,6 +3,7 @@
  */
 package sliding_window;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,10 +13,49 @@ import java.util.List;
  */
 public class CountAnagramsInString {
 	public static void main(String[] args) {
-		String txt = "cbaebabacd";
-		String pat = "abc";
+//		String txt = "cbaebabacd";
+		String txt = "aabaabaa";
+//		String pat = "abc";
+		String pat = "aaba";
 
 		List<Integer> result = countAnagrams(txt, pat);
 		System.out.println("Number of anagrams of pat in txt: " + result);
+	}
+	private static List<Integer> countAnagrams(String txt, String pat) {
+		List<Integer>result=new ArrayList<>();
+		int k=pat.length();
+		int[] count=new int[26];
+		for(char ch:pat.toCharArray()) {
+			count[ch-'a']++;
+		}
+		int i=0;
+		int j=0;
+		int n=txt.length();
+		while (j<n) {
+			int idx=txt.charAt(j)-'a';
+			count[idx]--;
+			if (j-i+1==k) {
+				if (allZero(count)) {
+					result.add(i);
+				}
+				count[txt.charAt(i)-'a']++;
+				i++;
+			}
+			j++;
+		}
+		return result;
+		
+	}
+	/**
+	 * @param count
+	 * @return
+	 */
+	private static boolean allZero(int[] count) {
+		for(int num:count) {
+			if (num!=0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
